1bbf71f140ebc4e2a26ab4f8d064dcff4550386a
Devika <devikab2021@gmail.com>
2022-10-03 16:54:19 +0530

    Idea Updation1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("openPop");
+
+}
+function closePopup(){
+    popup.classList.remove("openPop");
+    
+}
